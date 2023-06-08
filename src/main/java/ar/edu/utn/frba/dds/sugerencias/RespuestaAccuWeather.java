cc509008bd90ffeec3ae7c236597edcdd00f7b97
tobiascastaneda2000 <tobias.2000david@gmail.com>
2023-06-07 23:10:40 -0300

    commits iteracion 4 inicio

---
+++ src/main/java/ar/edu/utn/frba/dds/sugerencias/RespuestaAccuWeather.java
@@ -0,0 +1,21 @@
+package ar.edu.utn.frba.dds.sugerencias;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+// Esto es necesario solamente para "agregarle" a las condiciones
+// climáticas una expiración
+public class RespuestaAccuWeather {
+
+  Map<String, Object> estadoDelTiempo;
+  LocalDateTime expiracion;
+  public RespuestaAccuWeather(Map<String, Object> estadoDelTiempo, LocalDateTime expiracion) {
+    this.estadoDelTiempo = estadoDelTiempo;
+    this.expiracion = expiracion;
+  }
+
+  public boolean expiro() {
+    return this.expiracion.isAfter(LocalDateTime.now());
+  }
+}
---
+++ src/main/java/ar/edu/utn/frba/dds/sugerencias/ServicioMetereologico.java
@@ -0,0 +1,57 @@
+package ar.edu.utn.frba.dds.sugerencias;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServicioMetereologico {
+  Map<String, RespuestaAccuWeather> ultimasRespuestas;
+  AccuWeatherAPI api;
+  Duration periodoDeValidez;
+
+  // ¿es el API un componente pesado? ¿es un componente que podemos compartir?
+  // si instanciamos el objeto acá mismo, estamos asumiendo un montón de cosas
+  // (que es liviano, que no parece ser compartible),
+  // pero al usar inyección de dependencias evitamos
+  // hacer esas suposiciones: "pateamos el problema para adelante"
+  public ServicioMetereologico(AccuWeatherAPI api, Duration periodoDeValidez) {
+    this.api = api;
+    this.periodoDeValidez = periodoDeValidez;
+    this.ultimasRespuestas = new HashMap<String, Object>();
+  }
+
+  // esto es un algoritmo clásico de caché:
+  //     * si no está en caché o está expirada, recalcular y guardar.
+  //     * en cualquier caso: devolver el resultado de la caché
+  public Map<String, Object> obtenerCondicionesClimaticas(String direccion) {
+    if (
+        !this.ultimasRespuestas.contains(direccion)
+            || this.ultimasRespuestas.get(direccion).expiro()) {
+      ultimasRespuestas.put(new RespuestaAccuWeather(this.consultarApi(direccion), proximaExpiracion()));
+    }
+    return this.ultimasRespuestas.get(direccion).getEstadoDelTiempo();
+  }
+
+  private LocalDateTime proximaExpiracion() {
+    return LocalDateTime.now().plus(this.periodoDeValidez);
+  }
+
+  // optimización: acá podríamos guardar directamente las próxima predicciones y
+  // actualizar las cacheadas si las nuevas son diferentes
+  private Map<String, Object> consultarApi(String direccion) {
+    return this.api.getWeather(direccion).get(0);
+  }
+}
+
+
+
+
+  // getters
+
+  // si no fuera por este método en algunos lenguajes
+  // bien podría haber sido una tupla
+  public boolean expiro() {
+    return this.expiracion.isAfter(DateTime.now);
+  }
+}
